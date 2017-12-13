3bd4e807eb7808bcc40590a43d2a4196108aea1c
bastienwcs <bastien@wildcodeschool.fr>
2017-12-13 17:02:34 +0100

    fix

--- src/fr/wildcodeschool/wildmail/MailContentServlet.java
+++ src/fr/wildcodeschool/wildmail/MailContentServlet.java
@@ -18,21 +18,14 @@ public class MailContentServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         String id = request.getParameter("id");
-        boolean success = false;
-        if (id != null && !id.isEmpty()) {
-            List<MailBean> mailList = new ArrayList<>();
-            // TODO : récupère la liste des emails en session
+        List<MailBean> mailList = new ArrayList<>();
+        // TODO : récupère la liste des emails en session
 
 
 
-            int position = Integer.parseInt(id);
-            MailBean mailBean = mailList.get(position);
-            success = true;
-            request.setAttribute("mailBean", mailBean);
-            this.getServletContext().getRequestDispatcher("/mail_content.jsp").forward(request, response);
-        }
-        if (!success) {
-            response.sendRedirect(request.getContextPath() + "/mail/list");
-        }
+        int position = Integer.parseInt(id);
+        MailBean mailBean = mailList.get(position);
+        request.setAttribute("mailBean", mailBean);
+        this.getServletContext().getRequestDispatcher("/mail_content.jsp").forward(request, response);
     }
 }
